ecc9cb09e09f4157501e1032feae48dd18466424
Romil1510 <romil.rp1510@gmail.com>
2025-07-14 18:07:50 +0530

    cors

--- prediction_model/Server2.py
+++ prediction_model/Server2.py
@@ -2,8 +2,11 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import random
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
+
 @app.route('/predictionModel',methods=['POST'])
 
 def predictionModel():
